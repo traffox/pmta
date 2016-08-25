01e7fe025822c6925c61cd9e0a47c83b7db690ac
Uplusware <uplusware@gmail.com>
2016-08-25 16:09:45 +0800

    0.1
    0.1

--- ptx.py
+++ ptx.py
@@ -2,6 +2,7 @@ import socket
 import select
 import base64
 import os,re
+import dns.resolver
 
 class ptx():
     def __init__(self):
@@ -110,28 +111,14 @@ class ptx():
             return False
         
     def txmail(self, hostname, mailfrom, rcptto, bodystr):
-        mx_server_list = []
         mail_postfix = re.split('@',rcptto)
-        #print mail_postfix
-        try:
-            outstr = os.popen('nslookup -type=mx -timeout=10 %s'%mail_postfix[1], 'r').read()
-        except Exception, e:
-            print 'DEBUG: Execute nslookup:',e
-            return False
-        
-        linestr = re.split('\n', outstr)
-        for s in linestr:
-            if re.match('.+[ |\t]mail exchanger[ |\t].+', s) != None:
-                c = re.split(' |\t', s)
-                mx_server_list.append(c[len(c) - 1])
-
-        if len(mx_server_list) == 0:
+		mx_server_list = dns.resolver.query(mail_postfix[1], 'MX')
+		if len(mx_server_list) == 0:
             self.errmsg = 'Can not find MX server'
             return False
-        
-        for mx_element in mx_server_list:
-            return_val = True
-            mx_server_ip = socket.gethostbyname(mx_element)
+		for x in mx_server_list:
+			return_val = True
+            mx_server_ip = socket.gethostbyname(x.to_text())
             tx_sockfd = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
             try:
                 tx_sockfd.connect((mx_server_ip, 25))
@@ -152,7 +139,6 @@ class ptx():
             
             if return_val == True:
                 break
-            
         return return_val
     
 if __name__ == '__main__':
